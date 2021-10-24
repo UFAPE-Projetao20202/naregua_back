e421eb8772c69e1e1968cb4c0ae51dd6c0641ac5
Gabriel Castro <gabrielcssg@gmail.com>
2021-10-24 16:31:15 -0300

    adicionado tratamento de erros da aplicação

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.js
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.js
@@ -1,14 +1,15 @@
+const AppError = require('../../../../errors/AppError');
 const { UsersRepository } = require('../../repositories/UsersRepository');
 
 class CreateUserUseCase {
   async execute({ name, email, phone, password }) {
-    if (!name || !String(name).trim()) throw new Error('Informe o nome.');
+    if (!name || !String(name).trim()) throw new AppError('Informe o nome.');
 
     if (!password || !String(password).trim())
-      throw new Error('Informe a senha.');
+      throw new AppError('Informe a senha.');
 
     if ((!email || !String(email).trim()) && (!phone || !String(phone).trim()))
-      throw new Error('Informe o email ou telefone.');
+      throw new AppError('Informe o email ou telefone.');
 
     const usersRepository = new UsersRepository();
 
@@ -16,14 +17,14 @@ class CreateUserUseCase {
       const userEmailAlreadyExists = await usersRepository.findByEmail(email);
 
       if (userEmailAlreadyExists)
-        throw new Error('Já existe um usuário com esse email.');
+        throw new AppError('Já existe um usuário com esse email.');
     }
 
     if (phone) {
       const userPhoneAlreadyExists = await usersRepository.findByPhone(phone);
 
       if (userPhoneAlreadyExists)
-        throw new Error('Já existe um usuário com esse número de telefone.');
+        throw new AppError('Já existe um usuário com esse número de telefone.');
     }
 
     const user = await usersRepository.create({
