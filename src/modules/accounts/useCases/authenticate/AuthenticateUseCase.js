744eeea770a305850ba07b74fb80b688d956cd45
Gabriel Castro <gabrielcssg@gmail.com>
2021-10-31 16:42:29 -0300

    autenticacao de usuario

---
+++ src/modules/accounts/useCases/authenticate/AuthenticateUseCase.js
@@ -0,0 +1,38 @@
+const { compare } = require('bcryptjs');
+const { sign } = require('jsonwebtoken');
+
+const AppError = require('../../../../errors/AppError');
+
+class AuthenticateUseCase {
+  constructor(usersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  async execute({ email, password }) {
+    if (!email || !String(email).trim())
+      throw new AppError('Informe o email corretamente!');
+
+    if (!password || !String(password).trim())
+      throw new AppError('Informe a senha corretamente.');
+
+    // verifica se o usuário existe.
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) throw new AppError('Email ou senha incorretos!');
+
+    // compara as senhas.
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) throw new AppError('Email ou senha incorretos!');
+
+    // gera o token para autenticação.
+    const token = sign({}, '4f50d63afc971e7c1c98e62572ee489c', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user: { name: user.name, email: user.email }, token };
+  }
+}
+
+module.exports = { AuthenticateUseCase };
